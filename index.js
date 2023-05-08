b6d012ebc835a9db48b72e2569eca67209ae5e73
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-08 22:50:16 +0530

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const router = require("./Router/router");
+const http = require("http");
 const errorHandler = require("./Middleware/middleware");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +15,28 @@ app.use(
 );
 app.use("/api", router);
 app.use(errorHandler);
-app.listen(5000, (error) => {
+server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["POST", "PUT", "GET"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("your id", socket.id);
+  console.log(socket.id);
+  socket.on("send message", (body) => {
+    io.emit("message", body);
+  });
+  socket.on("disconnect", (data) => {
+    console.log(data);
+  });
+});
+
+server.listen(5000, (error) => {
   if (error) {
     console.log(error);
   }
