75e4f7756d3c050b54eb629abd98b187957ac5fd
sumns <sks23info@gmail.com>
2023-05-16 21:21:47 +0530

    Temperature Control App

---
+++ src/TemparatureControl.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const TemperatureControlApp = () => {
+  const [temperature, setTemperature] = useState(20);
+
+  const handleIncrement = () => {
+    setTemperature(prevTemperature => prevTemperature + 1);
+  };
+
+  const handleDecrement = () => {
+    setTemperature(prevTemperature => prevTemperature - 1);
+  };
+
+  let backgroundColor;
+  if (temperature >= 50) {
+    backgroundColor = 'red';
+  } else if (temperature >= 30) {
+    backgroundColor = 'orange';
+  } else {
+    backgroundColor = 'yellow';
+  }
+
+  return (
+    <div className="app" style={{ backgroundColor }}>
+      <h1>Temperature Control App</h1>
+      
+      <div className="temperature">
+        <button onClick={handleDecrement}> ➖ </button>
+        <span>{temperature}°C</span>
+        <button onClick={handleIncrement}> ➕ </button>
+      </div>
+    </div>
+  );
+};
+
+export default TemperatureControlApp;
