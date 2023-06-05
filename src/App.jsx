07bda574526851bbf959ddf49f19d255fbca1c8d
sumns <sks23info@gmail.com>
2023-06-05 23:04:18 +0530

    all the student data shown in table

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,97 @@
-
-import './App.css'
-import TemperatureControlApp from './TemparatureControl'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [data, setData] = useState([]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const studentInfo = {
+      name: name,
+      age: age,
+      phone: phone,
+      address: address,
+    };
+
+    setData([...data, studentInfo]);
+
+    setName("");
+    setAge("");
+    setPhone("");
+    setAddress("");
+  };
 
   return (
-    <>
-
-
-      
-    </>
-  )
+    <div className="container">
+      <h1>Student Form</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-group">
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Age:</label>
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Phone:</label>
+            <input
+              type="number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="">Address:</label>
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+        </div>
+        <button type="submit" className="form-button">
+          Submit
+        </button>
+      </form>
+      <div className="table">
+        <table className="data-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Phone</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((student, index) => (
+              <tr key={index}>
+                <td>{student.name}</td>
+                <td>{student.age}</td>
+                <td>{student.phone}</td>
+                <td>{student.address}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
