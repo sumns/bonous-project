e709222e0398331eade0e091b52a11848d0a181e
sumns <sks23info@gmail.com>
2023-06-12 23:52:48 +0530

    emoji search app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+import React, { useState } from "react";
+import emojiDictionary from "emoji-dictionary";
+import "./App.css";
 
-import './App.css'
-import TemperatureControlApp from './TemparatureControl'
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-function App() {
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
+  const getEmoji = () => {
+    const emoji = emojiDictionary.getUnicode(searchTerm);
+    if (emoji) {
+      return emoji;
+    } else {
+      return "Emoji not found";
+    }
+  };
 
   return (
-    <>
+    <div className="App">
+      <div className="container">
+        <h1>Emoji Search App</h1>
+        <input
+          type="text"
+          placeholder="Search for an emoji"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <h1>{getEmoji()}</h1>
+      </div>
+    </div>
+  );
+};
 
-
-      
-    </>
-  )
-}
-
-export default App
+export default App;
